9730dc1c0493ae34d1b2879f99cdd10946700beb
pawanmukati <pawanmukati765@gmail.com>
2023-07-03 19:06:25 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+
+
+
+var Swipes = new Swiper('.swiper-container', {
+    loop: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+    },
+});
+// ---
+// function toggleSection() {
+//     var section = document.getElementById("mySection");
+//     section.classList.toggle("hidden");
+//     section.style.maxHeight = section.classList.contains("hidden") ? null : section.scrollHeight + "px";
+//   }
+  
+  
+
+
+  
+  
